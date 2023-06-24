4349ad93f2471d28782193ce4c4fc4e24306d1f6
UmangBasuthkar <118044656+UmangBasuthkar@users.noreply.github.com>
2023-06-24 22:54:04 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -2,31 +2,60 @@ from flask import Flask, render_template, request, jsonify
 import pickle
 import pandas as pd
 import numpy as np
-import sklearn
 
 app = Flask(__name__, static_folder='static')
 
-def load_model(filename):
-    with open(filename, 'rb') as f:
+def get_height():
+    with open('height.pkl', 'rb') as f:
         model = pickle.load(f)
     return model
 
-model_height = load_model('height.pkl')
-model_subpopulation = load_model("subpopulation.pkl")
-model_yield = load_model("yield.pkl")
-accuracy = 94
-r2score = 94
-r2scorey = 86
+def get_subpopulation():
+    with open("subpopulation.pkl", "rb") as f:
+        model = pickle.load(f)
+    return model
 
+def get_yield():
+    with open("yield.pkl", "rb") as f:
+        model = pickle.load(f)
+    return model
+
+def get_accuracy():
+    accuracy_score = 94
+    return accuracy_score
+
+def get_r2score():
+    r2score = 94
+    return r2score
+
+def get_r2score_y():
+    r2scorey = 86
+    return r2scorey
 
 def conversion(input_sequence):
-    primary = pd.read_csv("Primary.csv")
-    primary = np.array(primary).reshape(-1)
+    primary = pd.read_csv("E:/ProjectWork/Agri-Genomics/Data/ML/Primary.csv")
+    primary = np.array(primary)
+    primary = primary.reshape(-1)
 
     #Conversion of string to float
+    input_list = list(input_sequence)
+    for i in range(len(input_sequence)):
+        if input_sequence[i] == "N":
+            input_list[i] = primary[i]
+    final_sequence = ''.join(input_list)
     legend = {'A': 1, 'T': 2, 'C': 3, 'G': 4}
-    final_sequence = [str(legend.get(base, primary[i])) for i, base in enumerate(input_sequence) if base != 'N']
-    return int(''.join(final_sequence))
+    string = ""
+    for i in final_sequence:
+        for j in i:
+            string += str(legend[j])
+    return int(string)
+
+model_height = get_height()
+model_subpopulation = get_subpopulation()
+model_yield = get_yield()
+accuracy = get_accuracy()
+r2score = get_r2score()
+r2scorey = get_r2score_y()
 
 
 @app.route('/')
@@ -44,15 +73,19 @@ def viewdata():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    if request.method == 'POST':        
+    if request.method == 'POST':
         gene_sequence = request.form['gene_sequence']
         final_sequence = conversion(gene_sequence)
 
-        input_array = np.array([[final_sequence]])
-        predicted_height = model_height.predict(input_array)[0]
-        predicted_subpopulation = model_subpopulation.predict(input_array)[0]
-        predicted_yield = model_yield.predict(input_array)[0]
 
+        predicted_height = model_height.predict([[final_sequence]])
+        predicted_height = predicted_height[0]
+
+        predicted_subpopulation = model_subpopulation.predict([[final_sequence]])
+        predicted_subpopulation = predicted_subpopulation[0]
+
+        predicted_yield = model_yield.predict([[final_sequence]])
+        predicted_yield = predicted_yield[0]
 
         return jsonify({'height': predicted_height, 'subpopulation' : predicted_subpopulation, 'sequence':gene_sequence, 'acc':accuracy, 'r2':r2score, 'yield':predicted_yield, 'r2y':r2scorey})
 
